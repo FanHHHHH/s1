b078e594b7f8f0ceba15b5f2528ae291eb503292
hef <hefan813@vip.qq.com>
2022-04-29 22:41:19 +0800

    构建httpserver中server模块

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,14 +1,8 @@
-use std::io::{Read, Write};
-use std::net::TcpListener;
+mod router;
+mod server;
+
+use server::Server;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
-
-    for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
-        println!("连接建立成功！");
-        let mut buffer = [0; 1024];
-        stream.read(&mut buffer).unwrap();
-        stream.write(&mut buffer).unwrap();
-    }
+    Server.run();
 }
